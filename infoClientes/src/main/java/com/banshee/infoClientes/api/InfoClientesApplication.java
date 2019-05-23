ea5c7df8dc0a7b8a0ef531e9c9d569e658e85090
alandres7 <alandres7@gmail.com>
2019-05-22 20:15:42 -0500

    Proyecto para prueba de actitud. Project Aptitude test

---
+++ infoClientes/src/main/java/com/banshee/infoClientes/api/InfoClientesApplication.java
@@ -0,0 +1,13 @@
+package com.banshee.infoClientes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoClientesApplication.class, args);
+	}
+
+}
