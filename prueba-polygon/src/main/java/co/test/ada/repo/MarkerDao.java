b706f4cc73f70622af1d432c3fed7d801e125ad5
alandres7 <alandres7@gmail.com>
2019-06-12 17:59:33 -0500

    No repository

--- prueba-polygon/src/main/java/co/test/ada/repo/MarkerDao.java
+++ prueba-polygon/src/main/java/co/test/ada/repo/MarkerDao.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import co.test.ada.entity.Marker;
 
-@Repository
 public interface MarkerDao extends CrudRepository<Marker, Long> {
 	
 	Marker findByNombre(String nombre);
